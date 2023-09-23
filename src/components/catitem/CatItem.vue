4ff90dbe9b5eff940366aef050353cb07a12afe6
Kristina <kristina.ferm@gmail.com>
2023-09-23 21:29:09 +0200

    Cat item start

---
+++ src/components/catitem/CatItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cat-item">
+    <img :src="`https://cdn2.thecatapi.com/images/${imageReference}.jpg`" />
+    <span>{{ name }}</span>
+    <span>{{ origin }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
+
+defineProps<{
+  imageReference: string;
+  name: string;
+  origin: string;
+}>();
+
+</script>
--- src/pages/breeds/Breeds.vue
+++ src/pages/breeds/Breeds.vue
@@ -1,39 +1,46 @@
 <template>
   <section class="breeds">
     <h1>Find out more</h1>
-    <Dropdown id="breed-selector" label="Pick a breed" :options="options" @select="onSelect" />
-    <div>
-      <span>Selected breed:</span> {{ store.getSelectedBreed }}
-    </div>
+    <Dropdown
+      id="breed-selector"
+      label="Pick a breed"
+      :options="options"
+      @select="onSelect"
+    />
+    <div v-if="store.getSelectedBreed.id !== ''">
+      <div>Selected breed: {{ selectedBreed.name }}</div>
+      <CatItem :imageReference="selectedBreed.reference_image_id" :name="selectedBreed.name" :origin="selectedBreed.origin" />
+    </div>    
   </section>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { useCatService } from '../../services/cat-service';
-import { useStore } from '../../store';
-import { IBreed } from '../../interfaces/IBreed';
-import Dropdown from '../../components/dropdown/Dropdown.vue'
+import { ref, computed } from "vue";
+import { useCatService } from "../../services/cat-service";
+import { useStore } from "../../store";
+import { IBreed } from "../../interfaces/IBreed";
+import CatItem from "../../components/catitem/CatItem.vue";
+import Dropdown from "../../components/dropdown/Dropdown.vue";
 
 const { getBreed } = useCatService();
 
 const store = useStore();
-
 const breeds = computed(() => store.breeds);
+const selectedBreed = computed(() => store.getSelectedBreed);
 
 const options = computed(() => {
   return breeds?.value?.map((breed) => ({
     label: breed.name,
-    value: breed.id
+    value: breed.id,
   }));
 });
 
 const onSelect = (value: string) => {
-  const breed = getBreed(value);  
+  const breed = getBreed(value);
   if (breed) {
     store.setSelectedBreed(breed);
-  }  
-}
+  }
+};
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 </script>
